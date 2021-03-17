cd45897090e1124f116722537a83c2f0f77a9c5e
Muhammad Masum Miah <masum07@hotmail.co.uk>
2021-03-17 19:50:03 +0000

    commented Tobi's work to make usable while javafx bug is fixed

--- BAPERS/src/BapersControl/Main.java
+++ BAPERS/src/BapersControl/Main.java
@@ -1,35 +1,35 @@
-package BapersControl;
-
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-import java.io.IOException;
-
-public class Main extends Application {
-    private static Stage stg;
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        stg = primaryStage;
-        primaryStage.setResizable(false);
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root, 520, 400));
-        primaryStage.show();
-    }
-
-    public void changeScene(String fxml) throws IOException {
-        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
-        stg.getScene().setRoot(pane);
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
+//package BapersControl;
+//
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//import javafx.stage.StageStyle;
+//
+//import java.io.IOException;
+//
+//public class Main extends Application {
+//    private static Stage stg;
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception{
+//        stg = primaryStage;
+//        primaryStage.setResizable(false);
+//        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
+//        primaryStage.setScene(new Scene(root, 520, 400));
+//        primaryStage.show();
+//    }
+//
+//    public void changeScene(String fxml) throws IOException {
+//        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+//        stg.getScene().setRoot(pane);
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
--- BAPERS/src/Database/DBConnection.java
+++ BAPERS/src/Database/DBConnection.java
@@ -1,25 +1,25 @@
-package Database;
-
-
-package BAPERS.Database;
-
-import java.sql.DriverManager;
-import java.sql.*;
-
-// this class is to connect to the databse
-public class DBConnection {
-    public Connection databaselink;
-
-    public Connection getConnection(){
-        try {
-            databaselink = DriverManager.getConnection("jdbc:sqlite:/Users/tobiadewunmi/Desktop/BAPERS-database/BAPERS.db");
-
-        }catch (Exception e){
-            e.printStackTrace();
-            e.getCause();
-        }
-
-        return databaselink;
-    }
-
-}
+//package Database;
+//
+//
+//
+//
+//import java.sql.DriverManager;
+//import java.sql.*;
+//
+//// this class is to connect to the databse
+//public class DBConnection {
+//    public Connection databaselink;
+//
+//    public Connection getConnection(){
+//        try {
+//            databaselink = DriverManager.getConnection("jdbc:sqlite:/Users/tobiadewunmi/Desktop/BAPERS-database/BAPERS.db");
+//
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            e.getCause();
+//        }
+//
+//        return databaselink;
+//    }
+//
+//}
